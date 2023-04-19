d8b6bda23a66748906571227434b55bda617becd
saisubhash9 <saisubhash654@gmail.com>
2023-04-19 19:39:45 -0400

    final

--- nobel.py
+++ nobel.py
@@ -32,7 +32,6 @@ def list_nations():
 
     nation = []
     for row in qres:
-        print(row)
         name = str(row.nation).split('/')[-1]
         nation.append(name)
     return jsonify(nation)
@@ -58,8 +57,8 @@ def list_categories():
     for row in qureyresult:
         category = str(row.wonprice).split('/')[-4]
         unique_categories.add(category)
-    
-    return sorted(list(unique_categories))
+    unique_categories=list(unique_categories)    
+    return jsonify(unique_categories)
 
 
 #GET: Return sorted years nobels are awarded
@@ -83,8 +82,9 @@ def list_years():
     for row in qureyresult:
         year = str(row.wonprice).split('/')[-2]
         unique_year.add(year)
+    unique_year=list(unique_year)
     
-    return sorted(list(unique_year))
+    return jsonify(unique_year)
 
 #GET: Return list of all nobel winners for the given year
 @app.route("/nobel/", methods=['GET', 'POST'])
@@ -112,7 +112,7 @@ def list_winner_year():
             #print(row)
             name = str(row.name)
             winners.append(name)
-        return winners
+        return jsonify(winners)
     
     if year and category:
 
@@ -145,11 +145,11 @@ def list_winner_nation():
                 winner_name = row[0] + " " + row[1]
                 winner_name = winner_name.split("http", 1)[0]
                 winners.append(winner_name)
-
-        return winners
+        print(winners)
+        return jsonify(winners)
 
 #GET: Return list of all nobel winners for the given category
-@app.route("/nobel/categorysearch", methods=['GET', 'POST'])
+@app.route("/nobel/categorysearch/", methods=['GET', 'POST'])
 def list_winner_category():
     category = 'chemistry'
     qres = g.query(
@@ -174,11 +174,11 @@ def list_winner_category():
             winner_name = winner_name.split("http", 1)[0]
             winners.append(winner_name)
     #print(len(winners))
-    return winners
+    return jsonify(winners)
 
 
 #GET: Return list of all nobel winners for the given year and category
-#@app.route("/nobel/cat_year_search", methods=['GET', 'POST'])
+# @app.route("/nobel/cat_year_search/", methods=['GET', 'POST'])
 def list_winner_year_category(year,category):
     category = category
     year = year
@@ -202,7 +202,107 @@ def list_winner_year_category(year,category):
             winner_name = row[0] + " " + row[1]
             winner_name = winner_name.split("http", 1)[0]
             winners.append(winner_name)
-    return winners
+    return jsonify(winners)
+
+@app.route("/nobeldetails/")
+def getDetails():
+#GET: Return details of the given nobel winner
+    name = request.args.get('name')
+    result = {
+        "Name": name,
+        "Nationality": 'Not Found',
+        "Category": 'Not Found',
+        "Nobel Awarded Year": 'Not Found',
+        "Association": 'Not Found',
+        "Birth Year": 'Not Found',
+        "Death Year": 'Still Alive',
+        "Motivation": 'Not Found',
+        "Photo": 'Not Found'
+    }
+    permdata = g.query(
+    """
+    PREFIX table:<http://swat.cse.lehigh.edu/resources/onto/nobel.owl#>
+    PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>
+    SELECT ?name ?nationality ?biryear ?photo ?category ?wonyear
+    {
+        ?g rdf:type table:PersonWinner;
+        table:name ?name;
+        table:nationality ?nationality;
+        table:birthYear ?biryear;
+        table:photo ?photo;
+        table:WonPrize ?category.
+        ?category table:yearWon ?wonyear;
+        
+    }
+    """)
+   
+    for row in permdata:
+        if name in str(row.asdict()['name'].toPython()):
+            result["Nationality"] = str(row.asdict()['nationality'].toPython()).split('/')[-1]
+            result["Birth Year"] = str(row.asdict()['biryear'].toPython())
+            result["Photo"] = str(row.asdict()['photo'].toPython())
+            result["Category"] = str(row.asdict()['category'].toPython()).split('/')[-4]
+            result["Nobel Awarded Year"] = str(row.asdict()['wonyear'].toPython())
+
+
+    assodata = g.query(
+        """
+        PREFIX table:<http://swat.cse.lehigh.edu/resources/onto/nobel.owl#>
+        PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>
+        SELECT ?name ?ass
+            {
+                ?g rdf:type table:PersonWinner;
+                table:name ?name;
+                table:Association ?ass.
+            }
+        """)
+
+    for row in assodata:
+        if name in str(row.asdict()['name'].toPython()):
+            result["Association"] = str(row.asdict()['ass'].toPython()).split('#',1)[-1]
+
+
+    deathyeardata = g.query(
+    """
+    PREFIX table:<http://swat.cse.lehigh.edu/resources/onto/nobel.owl#>
+    PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>
+    SELECT ?name ?deyear
+        {
+            ?g rdf:type table:PersonWinner;
+            table:name ?name;
+            table:deathYear ?deyear.
+        }
+    """)
+
+
+    for row in deathyeardata:
+        if name in str(row.asdict()['name'].toPython()):
+            result["Death Year"] = str(row.asdict()['deyear'].toPython())
+
+
+    motivationdata = g.query(
+        """
+        PREFIX table:<http://swat.cse.lehigh.edu/resources/onto/nobel.owl#>
+        PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>
+        SELECT ?name ?moti
+        {
+            ?g rdf:type table:PersonWinner;
+            table:name ?name;
+            table:WonPrize ?w.
+        ?w table:motivation ?moti;
+        }
+        """)
+
+    for row in motivationdata:
+        if name in str(row.asdict()['name'].toPython()):
+            result["Motivation"] = str(row.asdict()['moti'].toPython())
+    return render_template("details.html", name=name, nation=result["Nationality"], cate=result["Category"], awardyear=result["Nobel Awarded Year"], Asso=result["Association"], byear=result["Birth Year"], dyear=result["Death Year"], Motivation=result["Motivation"], Photo=result["Photo"] )
+
+
 
 if __name__ == "__main__":
     app.run()
