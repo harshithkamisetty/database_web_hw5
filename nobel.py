7093a56bf7f97b03f605b4f9681306487a8c9381
harshithkamisetty <52328387+harshithkamisetty@users.noreply.github.com>
2023-04-19 15:09:43 -0400

    second

--- nobel.py
+++ nobel.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import rdflib
 g = rdflib.Graph()
 result = g.parse("nobeldata.owl")
 print("graph has %s statements." % len(g))
 app = Flask(__name__)
 #apis
-#list_nations-GET: Return sorted names of all nations
-
+#list_nations-GET: Return sorted names of all nations(/nobel/nations)
+#list_categories-GET: Return sorted names of all nobel categories(/nobel/categories)
 #list_years-GET: Return sorted years nobels are awarded
 
 
+@app.route('/')
+def intor():
+   return render_template('index.html')
+
 #GET: Return sorted names of all nations
-#list_nations api
 @app.route('/nobel/nations')
 def list_nations():
     qres = g.query(
@@ -32,12 +35,12 @@ def list_nations():
         print(row)
         name = str(row.nation).split('/')[-1]
         nation.append(name)
-    return nation
+    return jsonify(nation)
 
 
 #GET: Return sorted names of all nobel categories
 @app.route('/nobel/categories')
-def sorted_names_categories():
+def list_categories():
     qureyresult = g.query(
             """
             PREFIX table:<http://swat.cse.lehigh.edu/resources/onto/nobel.owl#>
@@ -83,61 +86,71 @@ def list_years():
     
     return sorted(list(unique_year))
 
-
-def list_winner_year():
 #GET: Return list of all nobel winners for the given year
-    year = 1902
-
-    qres = g.query(
-        """
-        PREFIX table:<http://swat.cse.lehigh.edu/resources/onto/nobel.owl#>
-        PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>
-        SELECT ?name
-        {
-        ?g rdf:type table:PersonWinner;
-        table:name ?name;
-        table:WonPrize ?wonprice.
-        ?wonprice table:yearWon ?year.
-        FILTER (?year = """ + str(year) + """)
-        }""")
-
-    winners = []
-    for row in qres:
-        #print(row)
-        name = str(row.name)
-        winners.append(name)
-    return winners
-
-#GET: Return list of all nobel winners for the given nation
-def list_winner_nation():
-    nation = 'Argentina'
-
-    qres = g.query(
-        """
-        PREFIX table:<http://swat.cse.lehigh.edu/resources/onto/nobel.owl#>
-        PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>
-        SELECT ?name ?nationality
-        {
+@app.route("/nobel/", methods=['GET', 'POST'])
+def list_winner_year():
+    year = request.args.get('year')
+    category=request.args.get('category')
+    if year:
+        print("hello")
+        qres = g.query(
+            """
+            PREFIX table:<http://swat.cse.lehigh.edu/resources/onto/nobel.owl#>
+            PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>
+            SELECT ?name
+            {
             ?g rdf:type table:PersonWinner;
             table:name ?name;
-            table:nationality ?nationality.
-        }""")
+            table:WonPrize ?wonprice.
+            ?wonprice table:yearWon ?year.
+            FILTER (?year = """ + str(year) + """)
+            }""")
+
+        winners = []
+        for row in qres:
+            #print(row)
+            name = str(row.name)
+            winners.append(name)
+        return winners
+    
+    if year and category:
+
+        temp=list_winner_year_category(year,category)
+        return temp
 
 
-    winners = []
-    for row in qres:
-        if nation in row[1].split('/')[-1]:
-            winner_name = row[0] + " " + row[1]
-            winner_name = winner_name.split("http", 1)[0]
-            winners.append(winner_name)
+#GET: Return list of all nobel winners for the given nation
+@app.route("/nobel/nations/", methods=['GET', 'POST'])
+def list_winner_nation():
+    nation=request.args.get('nation')
+    if nation:
+        nation = nation
+        qres = g.query(
+            """
+            PREFIX table:<http://swat.cse.lehigh.edu/resources/onto/nobel.owl#>
+            PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>
+            SELECT ?name ?nationality
+            {
+                ?g rdf:type table:PersonWinner;
+                table:name ?name;
+                table:nationality ?nationality.
+            }""")
 
-    return winners
 
+        winners = []
+        for row in qres:
+            if nation in row[1].split('/')[-1]:
+                winner_name = row[0] + " " + row[1]
+                winner_name = winner_name.split("http", 1)[0]
+                winners.append(winner_name)
 
-def list_winner_category():
+        return winners
+
 #GET: Return list of all nobel winners for the given category
+@app.route("/nobel/categorysearch", methods=['GET', 'POST'])
+def list_winner_category():
     category = 'chemistry'
     qres = g.query(
         """
@@ -165,9 +178,10 @@ def list_winner_category():
 
 
 #GET: Return list of all nobel winners for the given year and category
-def list_winner_year_category():
-    category = 'chemistry'
-    year = '1901'
+#@app.route("/nobel/cat_year_search", methods=['GET', 'POST'])
+def list_winner_year_category(year,category):
+    category = category
+    year = year
     qres = g.query(
         """
         PREFIX table:<http://swat.cse.lehigh.edu/resources/onto/nobel.owl#>
@@ -190,3 +204,5 @@ def list_winner_year_category():
             winners.append(winner_name)
     return winners
 
+if __name__ == "__main__":
+    app.run()
